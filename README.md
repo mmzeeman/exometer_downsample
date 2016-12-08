b85c7ad0c073a8bb6b300b448548ae7bd9ace596
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2016-12-08 11:26:25 +0100

    Added sqlite transaction utility function

--- README.md
+++ README.md
@@ -18,3 +18,10 @@ The periods are:
 
 The samples will flow from the short period table to the longer period table. This means the resolution of the 
 samples will decrease over time.
+
+## Subscription
+
+When the reporter is subscribed to a metric the tables will be automatically created and the data will be stored
+in the tables. 
+
+When your metric is named [metric, test1] a table named "[metric,test1,hour]" stores the hour samples.
--- src/esqlite3_utils.erl
+++ src/esqlite3_utils.erl
@@ -1,11 +1,12 @@
 
 
-%% Handy utilities functions.
+%% Handy sqlite utilities functions.
 
 -module(esqlite3_utils).
 
 -export([
-    table_exists/2
+    table_exists/2,
+    transaction/2
 ]).
 
 % @doc Return true iff a table with TableName exists.
@@ -14,6 +15,32 @@ table_exists(TableName, Db) ->
         [{_BinTableName}] -> true;
         [] -> false
     end.
+
+% @doc transaction(Fun, Db)
+transaction(Fun, Db) ->
+    try
+        case esqlite3:q("BEGIN", Db) of
+            [] -> ok;
+            {error, _} = ErrorBegin -> throw(ErrorBegin)
+        end,
+
+        case Fun(Db) of
+            {rollback, Result} ->
+                esqlite3:q("ROLLBACK", Db),
+                Result;
+            Result ->
+                case esqlite3:q("COMMIT", Db) of
+                    [] -> Result;
+                    {error, _} = ErrorCommit -> 
+                        throw(ErrorCommit)
+                end
+        end
+    catch 
+        _:Why ->
+            esqlite3:q("ROLLBACK", Db),
+            {rollback, {Why, erlang:get_stacktrace()}}
+    end.
+
 
 %%
 %% Tests
@@ -28,6 +55,41 @@ table_exists_test() ->
 
     esqlite3:q("create table foo(\"a\" text)", Db),
     ?assertEqual(true, table_exists(foo, Db)),
+    ok.
+
+transaction_test() ->
+    {ok, Db} = esqlite3:open(":memory:"),
+
+    ?assertEqual(ok, transaction(fun(_TDb) -> ok end, Db)),
+    ?assertMatch({rollback, _}, transaction(fun(_TDb) -> 1/0 end, Db)),
+    ?assertEqual(ok, transaction(fun(_TDb) -> {rollback, ok} end, Db)),
+
+    %% Now some db changing things to see of things are actually rolled back
+    ?assertEqual([], transaction(fun(TDb) ->  
+        esqlite3:q("create table test(id integer primary key, name text)", TDb)
+    end, Db)),
+    %% Check if the commit is done.
+    ?assertEqual([], esqlite3:q("select * from test", Db)),
+
+    %% Now some db changing things to see of things are actually rolled back
+    ?assertMatch({rollback, _}, transaction(fun(TDb) ->  
+        esqlite3:q("create table test2(id integer primary key, name text)", TDb),
+        %% Simulate a bug in code...
+        1/0
+    end, Db)),
+
+    %% There should be no test2 table now.
+    ?assertThrow({error, {sqlite_error, "no such table: test2"}}, esqlite3:q("select * from test2", Db)),
+
+    ?assertEqual(oops, transaction(fun(TDb) ->  
+        esqlite3:q("create table test2(id integer primary key, name text)", TDb),
+        %% Explicitly rollback.
+        {rollback, oops}
+    end, Db)),
+
+    %% There should be no test2 table now.
+    ?assertThrow({error, {sqlite_error, "no such table: test2"}}, esqlite3:q("select * from test2", Db)),
+
     ok.
 
 -endif.
