a61d80888868f6ab9141eaf18f74518cebfdcd57
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2016-12-12 07:32:23 +0100

    More updates

--- test/exometer_sqlite_test.erl
+++ test/exometer_sqlite_test.erl
@@ -24,8 +24,10 @@ do_test() ->
     ?assertMatch([{exometer_report_sqlite, _}], exometer_report:list_reporters()),
 
     %exometer:update_or_create([metric, cpu], 1, cpu, []),
+
     exometer:update_or_create([metric, histogram], 1, histogram, []),
     exometer:update_or_create([metric, counter], 2, counter, []),
+    
     %exometer:update_or_create([metric, uniform], 2, uniform, []),
     %exometer:update_or_create([metric, gauge], 2, gauge, []),
     %exometer:update_or_create([metric, spiral], 2, spiral, []),
@@ -39,8 +41,6 @@ do_test() ->
     exometer:update([metric, counter], 13),
     exometer:update([metric, histogram], 13),
     exometer:update([metric, histogram], 75),
-
-    io:fwrite(standard_error, "m,h: ~p~n", [exometer:get_value([metric, histogram], [min, max])]),
 
     timer:sleep(100),
     exometer:update([metric, counter], 26),
